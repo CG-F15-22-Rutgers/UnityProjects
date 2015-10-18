60f0ce226c688c1b5e3cce0b28cdece4ce51793e
CG-F15-22-Rutgers <761315177@qq.com>
2015-10-19 02:59:12 +0800

    need answers for question 8,9 in part 1 a start scene is to be implemented

---
+++ BAssignments/B1/Assets/script/Director.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour
+{
+
+    public Vector3 target;
+    private NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        target = new Vector3(-1000, -1000, -1000);
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      
+             if (target != new Vector3(-1000, -1000, -1000))
+             {
+                agent.SetDestination(target);
+            }
+         
+       
+    }
+
+  
+
+}
